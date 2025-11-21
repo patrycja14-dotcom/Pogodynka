4c2919e92e2a929cc85e2778230aa3b0ddee9c03
Patrycja <patrycja14@gmail.com>
2025-11-21 02:51:48 +0100

    Final project version

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,10 @@
 // backend/src/db.js
 const { Pool } = require('pg');
 
-console.log('DATABASE_URL in db.js:', process.env.DATABASE_URL ?? '(BRAK)');
-
 const config = {};
 
 if (process.env.DATABASE_URL) {
   config.connectionString = process.env.DATABASE_URL;
-  // Render Postgres wymaga SSL
   config.ssl = { rejectUnauthorized: false };
 }
 
